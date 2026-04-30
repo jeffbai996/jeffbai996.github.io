2956fb7fdbc6eae0f79d5a84c94b2e4a3600ff3f
jeffbai996 <jeffbai996@users.noreply.github.com>
2026-04-30 04:02:32 -0700

    feat(spectrum): SpectrumLogo SVG component

---
+++ frontend/src/pages/spectrum/components/SpectrumLogo.jsx
@@ -0,0 +1,29 @@
+import PropTypes from 'prop-types'
+
+// Reverse-rainbow VIBGYOR Minecraft concrete colors, left to right
+const COLORS = ['#8932B8', '#3C44AA', '#3AB3DA', '#5E7C16', '#FED83D', '#F9801D', '#B02E26']
+
+export default function SpectrumLogo({ size = 'md', wordmark = true }) {
+  const sizeClass = `sp-logo-${size}`
+  return (
+    <span className={`sp-logo ${sizeClass}`} aria-label="Spectrum Cannabis">
+      <svg
+        className="sp-logo-bar"
+        viewBox="0 0 140 40"
+        xmlns="http://www.w3.org/2000/svg"
+        aria-hidden="true"
+        preserveAspectRatio="none"
+      >
+        {COLORS.map((c, i) => (
+          <rect key={c} x={i * 20} y="0" width="20" height="40" fill={c} />
+        ))}
+      </svg>
+      {wordmark && <span className="sp-logo-wordmark">spectrum cannabis</span>}
+    </span>
+  )
+}
+
+SpectrumLogo.propTypes = {
+  size: PropTypes.oneOf(['sm', 'md', 'lg']),
+  wordmark: PropTypes.bool
+}
